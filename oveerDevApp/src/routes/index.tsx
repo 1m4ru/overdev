ab12f6cb91bf5fd0cb0b433aa65a8be5f9879d27
Maruan Moussa <maruanibrahim22@gmail.com>
2023-05-17 14:10:30 -0300

    criado o elemento de rotas

---
+++ oveerDevApp/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+ 
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+
+            <Route path='*' element={<Navigate to="/login"/>} />
+        </Routes>
+    );
+
+}
